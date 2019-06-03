665da3395787275fc88d1a316e2376b4dae9b55a
matteocordiner <46818249+Kassianco@users.noreply.github.com>
2019-06-03 13:08:25 +0200

    updates

--- spd-menu.js
+++ spd-menu.js
@@ -1,8 +1,11 @@
 var menuBTN = document.getElementById("menuBTN");
+var menuBTNBack = document.getElementById("menuBTNBack");
+
 var navContainer = document.getElementById("navContainer");
 var oakleyLogo = document.getElementById("oakleyLogo");
 
 menuBTN.addEventListener('click', toggleTheMenu);
+menuBTNBack.addEventListener('click', toggleTheMenu);
 
 function toggleTheMenu() {
   console.log('hello');
